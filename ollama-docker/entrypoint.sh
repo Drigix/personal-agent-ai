f063eca22e11da917ece02389637e5935affd21f
Michał Ławinski <michrarr@gmail.com>
2025-12-14 12:44:24 +0100

    Add delay to ollama container

--- ollama-docker/entrypoint.sh
+++ ollama-docker/entrypoint.sh
@@ -5,6 +5,12 @@ echo "Starting ollama server..."
 ollama serve &
 SERVER_PID=$!
 
+echo "Waiting for ollama server to be ready..."
+until ollama list > /dev/null 2>&1; do
+  echo "Server not ready yet. Retrying in 5 seconds..."
+  sleep 5
+done
+
 echo "Pull model: ${MODEL}"
 ollama pull "${MODEL}"
 
