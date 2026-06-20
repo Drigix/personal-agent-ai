cf5988b16b2f62a80c2bfc1c1f3c02155c1cf2ab
Drigix <michrarr@gmail.com>
2026-06-20 21:37:32 +0200

    Create new application module for the users

---
+++ agent-ai-users/src/main/java/com/agent_ai_users/AgentAiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.agent_ai_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentAiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentAiUsersApplication.class, args);
+	}
+
+}
---
+++ agent-ai-users/src/test/java/com/agent_ai_users/AgentAiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent_ai_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentAiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
